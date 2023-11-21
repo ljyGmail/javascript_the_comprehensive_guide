8d7bf0ac6757bb80178cf656243efadb90fe1408
liangjinyong <liangjy881@gmail.com>
2023-11-21 11:59:11 +0900

    ch02 getting started

---
+++ ch02_getting_started/sec03_creating_output/02_writing_to_the_console/scripts/main.js
@@ -0,0 +1,7 @@
+function showMessage() {
+    console.log('Hello developer world'); // Print a standard message
+    console.debug('Hello developer world'); // Print a debug message
+    console.error('Hello developer world');  // Print an error message
+    console.info('Hello developer world'); // Print an info message
+    console.warn('Hello developer world'); // Print a warning message
+}
